ea05c3b58e28386cba23dae564fc15e0a02ab054
Shoaib Merchant <shoaibm@mechasystems.com>
2026-09-04 19:01:21 +0530

    drm: samsung-dsim: chipwealth-ch13726a: enables TE IRQ

--- drivers/gpu/drm/panel/panel-chipwealth-ch13726a.c
+++ drivers/gpu/drm/panel/panel-chipwealth-ch13726a.c
@@ -59,16 +59,45 @@ static void ch13726a_reset(struct ch13726a_panel *ctx)
 
 static int ch13726a_on(struct ch13726a_panel *ctx)
 {
-	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
+	struct mipi_dsi_multi_context dsi_ctx = {
+		.dsi = ctx->dsi,
+	};
 
 	ctx->dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, MIPI_DCS_WRITE_CONTROL_DISPLAY, MIPI_DCS_NOP);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, MIPI_DCS_EXIT_SLEEP_MODE, MIPI_DCS_NOP);
+	/* Vendor unlock */
+	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xF0, 0x50);
 
+	/* Vendor command mode enable */
+	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xB9, 0x11);
+
+	/* Full-frame update region */
+	mipi_dsi_dcs_set_column_address_multi(&dsi_ctx,
+					      0, 1079);
+
+	mipi_dsi_dcs_set_page_address_multi(&dsi_ctx,
+					    0, 1239);
+
+	/* Enable TE output  */
+    mipi_dsi_dcs_set_tear_on_multi(&dsi_ctx,
+			            MIPI_DSI_DCS_TEAR_MODE_VBLANK);
+
+	/* WRCTRLD */
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx,
+			     MIPI_DCS_WRITE_CONTROL_DISPLAY,
+			     0x00);
+
+	/* Sleep Out */
 	mipi_dsi_dcs_exit_sleep_mode_multi(&dsi_ctx);
+	mipi_dsi_msleep(&dsi_ctx, 120);
 
+	/* Display On */
 	mipi_dsi_dcs_set_display_on_multi(&dsi_ctx);
+	mipi_dsi_msleep(&dsi_ctx, 20);
+
+	dev_info(&ctx->dsi->dev,
+		 "command-mode init ret=%d\n",
+		 dsi_ctx.accum_err);
 
 	return dsi_ctx.accum_err;
 }
@@ -304,8 +333,7 @@ static int ch13726a_probe(struct mipi_dsi_device *dsi)
 
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
-			  MIPI_DSI_CLOCK_NON_CONTINUOUS;
+	dsi->mode_flags =  MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
 	ctx->panel.prepare_prev_first = true;
 
